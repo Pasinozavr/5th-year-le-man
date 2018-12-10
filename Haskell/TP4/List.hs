11f453a36363ea2d579379d0d40c5845135db0ef
Pavlo Andriiash <96pasha@ukr.net>
2018-12-10 16:43:42 +0100

    Add files via upload

--- Haskell/TP4/List.hs
+++ Haskell/TP4/List.hs
@@ -77,6 +77,12 @@ suffixes1 xs = suffixes1_temp xs [] where
 	suffixes1_temp xs [] = suffixes1_temp xs [xs]
 	suffixes1_temp (xs:xl) res = suffixes1_temp xl (xl:res)
 
+conjugue mot num = 
+	if num > longueur mot then error "life is too cruel"
+		else conjugue_temp mot num where
+			conjugue_temp mot num = if num <= longueur mot then  [nthelement mot num] ++ conjugue_temp (delete (nthelement mot num) mot) num
+				else mot
+
 inferieur a b =  a <= b 
 
 nthelement xs num = el_temp xs num 1 where
@@ -84,21 +90,28 @@ nthelement xs num = el_temp xs num 1 where
 	el_temp (el:xs) num temp =
 		if num == temp then el else el_temp xs num (temp+1)
 
-conjugue mot num = 
-	if num > longueur mot then error "life is too cruel"
-		else conjugue_temp mot num where
-			conjugue_temp mot num = if num <= longueur mot then  [nthelement mot num] ++ conjugue_temp (delete (nthelement mot num) mot) num 
-				else mot
+lyndon_extra mot = if nthelement mot (longueur mot) == 0 then False else True
 
-lyndon mot = lyndon_temp mot 2 where
-	lyndon_temp mot num = if num <= longueur mot then 
+lyndon_temp mot num = if num <= longueur mot then
 		inferieur mot (conjugue mot num) && lyndon_temp mot (num+1)
 		else True
 
+lyndon mot = lyndon_extra mot && lyndon_temp mot 2
+		
 consist ar el = consist_temp ar el 1 where
 	consist_temp ar el num = if num <= longueur ar then
 		el == nthelement ar num || consist_temp ar el (num+1)
 		else False 
+
+fusion_liste l1 l2 = fusion_temp l1 l2 [] where
+	fusion_temp [] b res = tri(res)
+	fusion_temp (a:as) b res = b_temp a b 1 res where
+		b_temp a b temp res =
+			if temp <= longueur b then
+				let one = a++ nthelement b temp in
+				if lyndon one && not (consist res one) then b_temp a b (temp+1) (one:res)
+					else b_temp a b (temp+1) res
+			else fusion_temp as b res
 
 insere_liste l1 l2 = insere_temp l1 l2 [] where
 	insere_temp [] [] res = res
@@ -114,27 +127,11 @@ insere_liste l1 l2 = insere_temp l1 l2 [] where
 				else if not (consist res x) && consist res y then insere_temp xs ys (x:res)
 					else insere_temp xs ys (y:res)
 
-
-fusion_liste l1 l2 = fusion_temp l1 l2 [] where
-	fusion_temp [] b res = res
-	fusion_temp (a:as) b res = b_temp a b 1 res where
-		b_temp a b temp res =
-			if temp <= longueur b then
-				let one = a++ nthelement b temp in
-				if lyndon one && not (consist res one) then b_temp a b (temp+1) (one:res)
-					else b_temp a b (temp+1) res
-			else fusion_temp as b res
-
-fusion_mot u l = case l of
-{
-	[] -> [];
-	v:r -> if inferieur u v then (u++v):(fusion_mot u r) else fusion_mot u r;
-} 
-fusion_lst l1 l2 = case l1 of
-{
-	[] -> [];
-	u:r -> insere_liste (fusion_mot u l2) (fusion_lst r l2);
-}
+genere n = if n == 1 then [[0],[1]] else if n > 1 then 
+	insere_liste
+	(fusion_liste (genere 1) (genere (n - 1))) 
+	(fusion_liste (genere (n - 1)) (genere 1)) 
+	else error "auch"
 
 main = do
 	let liste = [1, 3, 4, 2, 5, 12]
@@ -153,38 +150,41 @@ main = do
 	--print("added 2 = " ++ show(newnewlist))
 	--let concatedlist = conc liste newnewlist
 	--print(show(liste) ++ " @ " ++ show(newnewlist) ++ " = " ++ show(concatedlist))
-	let inve = inverse liste
-	print("inverse for " ++ show(liste) ++ " = " ++ show(inve))
-	print("max1 in list = " ++ show(max_un liste))
-	print("max2 in list = " ++ show(max_deux liste))
-	print("max3 in list = " ++ show(max_trois liste))
-	print("enter value for inf (intervalle_asc): ")
-	input1 <- getLine
-	let a = (read input1 :: Int)
-	print("enter value for sup (intervalle_asc): ")
-	input2 <- getLine
-	let b = (read input2 :: Int)
-	print("liste asc " ++ show(a) ++ "->" ++ show (b) ++ " = " ++ show (intervalle_asc a (b+1)))
-	print("liste desc " ++ show(b) ++ "->" ++ show (a) ++ " = " ++ show (intervalle_desc (b+1) a))
-	let liste = "abc"
-	let liste1 = [1,2,3,4,5]
-	let liste2 = [[1,2],[3,4],[5,6]]
-	print("Prefixes = " ++ show(prefixes liste)) 
-	print("Suffixes1 = " ++ show(suffixes1 liste))
-	print("Prefixes = " ++ show(prefixes liste1)) 
-	print("Suffixes1 = " ++ show(suffixes1 liste1))
-	print("Prefixes = " ++ show(prefixes liste2)) 
-	print("Suffixes1 = " ++ show(suffixes1 liste2))
-	print("bierre < bonbon? " ++ show(inferieur ['b', 'i', 'e', 'r', 'e'] ['b', 'o', 'n', 'b', 'o', 'n']))
-	print("cierre < bonbon? " ++ show(inferieur ['c', 'i', 'e', 'r', 'e'] ['b', 'o', 'n', 'b', 'o', 'n']))
-	print("3rd el of abc = " ++ show (nthelement liste 3))
-	print(show("lyndon of aaab? " ++ show(lyndon ['a','a','a','b'])))
-	print(show("lyndon of abab? " ++ show(lyndon ['a','b','a','b'])))
-	print(show("lyndon of baab? " ++ show(lyndon ['b','a','a','b'])))
-	print(show("lyndon of aaba? " ++ show(lyndon ['a','a','b','a'])))
-	print("consist for qwe w =" ++ show(consist ['q','w', 'e'] 'w'))
-	print("insere1 = " ++ show(insere_liste [['0', '1'], ['1', '0']] [['0','0','1'],['0','1']]))
-	print("insere2 = " ++ show(insere_liste [['0'], ['1']] [['0','0','1'],['0','1']]))
-	print(show(tri(fusion_liste [['0','0']] [['0','0','1'],['0','1'],['1','0'],['1','1']])))
-	print(show(tri(fusion_lst [['0','0']] [['0','0','1'],['0','1'],['1','0'],['1','1']])))
-	print("conjugure for a b c d 5 = " ++ show (conjugue ['a', 'b', 'c', 'd'] 5))
+	--let inve = inverse liste
+	--print("inverse for " ++ show(liste) ++ " = " ++ show(inve))
+	--print("max1 in list = " ++ show(max_un liste))
+	--print("max2 in list = " ++ show(max_deux liste))
+	--print("max3 in list = " ++ show(max_trois liste))
+	--print("enter value for inf (intervalle_asc): ")
+	--input1 <- getLine
+	--let a = (read input1 :: Int)
+	--print("enter value for sup (intervalle_asc): ")
+	--input2 <- getLine
+	--let b = (read input2 :: Int)
+	--print("liste asc " ++ show(a) ++ "->" ++ show (b) ++ " = " ++ show (intervalle_asc a (b+1)))
+	--print("liste desc " ++ show(b) ++ "->" ++ show (a) ++ " = " ++ show (intervalle_desc (b+1) a))
+	--let liste = "abc"
+	--let liste1 = [1,2,3,4,5]
+	--let liste2 = [[1,2],[3,4],[5,6]]
+	--print("Prefixes = " ++ show(prefixes liste)) 
+	--print("Suffixes1 = " ++ show(suffixes1 liste))
+	--print("Prefixes = " ++ show(prefixes liste1)) 
+	--print("Suffixes1 = " ++ show(suffixes1 liste1))
+	--print("Prefixes = " ++ show(prefixes liste2)) 
+	--print("Suffixes1 = " ++ show(suffixes1 liste2))
+	--print("bierre < bonbon? " ++ show(inferieur ['b', 'i', 'e', 'r', 'e'] ['b', 'o', 'n', 'b', 'o', 'n']))
+	--print("cierre < bonbon? " ++ show(inferieur ['c', 'i', 'e', 'r', 'e'] ['b', 'o', 'n', 'b', 'o', 'n']))
+	--print("3rd el of abc = " ++ show (nthelement liste 3))
+	--print(show("lyndon of aaab? " ++ show(lyndon ['a','a','a','b'])))
+	--print(show("lyndon of abab? " ++ show(lyndon ['a','b','a','b'])))
+	--print(show("lyndon of baab? " ++ show(lyndon ['b','a','a','b'])))
+	--print(show("lyndon of aaba? " ++ show(lyndon ['a','a','b','a'])))
+	--print("consist for qwe w =" ++ show(consist ['q','w', 'e'] 'w'))
+	--print("insere1 = " ++ show(insere_liste [['0', '1'], ['1', '0']] [['0','0','1'],['0','1']]))
+	--print("insere2 = " ++ show(insere_liste [['0'], ['1']] [['0','0','1'],['0','1']]))
+	--print("conjugure for a b c d 5 = " ++ show (conjugue ['a', 'b', 'c', 'd'] 5))
+	print("genere 1 : " ++ show(genere 1))
+	print("genere 2 : " ++ show(genere 2))
+	print("genere 3 : " ++ show(genere 3))
+	print("genere 4 : " ++ show(genere 4))
+	print("genere 5 : " ++ show(genere 5))
